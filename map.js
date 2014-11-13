b286779b5e6f05adaf3ff03b91d598b78cc7d417
oborny <alexoborny@tamu.edu>
2014-11-13 13:52:34 -0600

    added a map to dis hoe

---
+++ map.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Alex on 11/13/2014.
+ */
+function initialize() {
+    var myLatlng = new google.maps.LatLng(30.61,-96.34);
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+    var marker1 = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Hello World!',
+        icon: "static/badouthouse.png"
+    });
+    var marker = new google.maps.Marker({
+        position: {lat:31.61,lng:-96.34},
+        map: map,
+        title: 'Hello World!',
+        icon: "static/goodouthouse.png"
+    });
+    var infowindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,myLatlng);
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
