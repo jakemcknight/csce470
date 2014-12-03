f315816d2c1f98a8d5f3bca558ae26ee1a1f8017
Miles Hickman <mileshick@gmail.com>
2014-12-03 12:55:11 -0600

    Added api stub for getting all bathrooms

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 __author__ = 'Miles Hickman'
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('bathroombuddy.html')
+
+@app.route('/api/get_all')
+def get_all_bathrooms():
+    abort(418)
 
 if __name__ == '__main__':
     app.run(debug=True)
