a51fb0a2a9d8002e1107bc283d3420a6ea41949b
Miles Hickman <mileshick@gmail.com>
2014-12-03 12:49:58 -0600

    Added basic flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+__author__ = 'Miles Hickman'
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('bathroombuddy.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
