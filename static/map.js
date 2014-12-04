738a540c87ab746edf6ca54e607403a492d77130
oborny <alexoborny@tamu.edu>
2014-12-03 20:03:03 -0600

    stuff

--- static/map.js
+++ static/map.js
@@ -1,10 +1,10 @@
 function initialize() {
-    var myLatlng = new google.maps.LatLng(30.61,-96.34);
+  var myLatlng = new google.maps.LatLng(30.61,-96.34);
     var mapOptions = {
         center: myLatlng,
         zoom: 8
     };
-    var map = new google.maps.Map(document.getElementById('map-container'),
+  var map = new google.maps.Map(document.getElementById('map-container'),
         mapOptions);
     var marker1 = new google.maps.Marker({
         position: myLatlng,
@@ -18,9 +18,23 @@ function initialize() {
         title: 'Hello World!',
         icon: "../static/goodouthouse.png"
     });
-    var infowindow = new google.maps.InfoWindow();
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(marker,myLatlng);
-    });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+//console.log("you dawg i heard you like maps")
+// var marker5 = new google.maps.Marker({
+//        position: {lat:31.69,lng:-99.34},
+//        map: map,
+//        title: 'Hello!',
+//        icon: "../static/goodouthouse.png"
+//    });
+function get_data() {
+    $(document).ready(function(){
+    $("button").click(function(){
+    $.getJSON("/api/get_all",function(result){
+    $.each(result, function(i, field){
+    console.log(result)
+});
+});
+});
+});
+}
