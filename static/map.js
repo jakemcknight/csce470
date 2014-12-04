01a9be3b04fe5035c178eb9ca4ab3df6b9cd567f
oborny <alexoborny@tamu.edu>
2014-12-03 21:23:30 -0600

    we are doing stuff in a big way!

--- static/map.js
+++ static/map.js
@@ -1,10 +1,12 @@
+var map
+var myLatlng
 function initialize() {
-  var myLatlng = new google.maps.LatLng(30.61,-96.34);
+   myLatlng = new google.maps.LatLng(30.61,-96.34);
     var mapOptions = {
         center: myLatlng,
         zoom: 8
     };
-  var map = new google.maps.Map(document.getElementById('map-container'),
+   map = new google.maps.Map(document.getElementById('map-container'),
         mapOptions);
     var marker1 = new google.maps.Marker({
         position: myLatlng,
@@ -28,6 +30,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 //        icon: "../static/goodouthouse.png"
 //    });
 function get_data() {
+    console.log("test")
     $(document).ready(function(){
     $("button").click(function(){
     $.getJSON("/api/get_all",function(result){
@@ -38,3 +41,34 @@ function get_data() {
 });
 });
 }
+  function addMarker() {
+        $.getJSON("/api/get_all",function(data){
+        $.each(data,function(key,value){
+           var x=value.latitude
+           var y=value.longitude
+           var string = value.name
+           var string1= "is a sexy motherfucker"
+           var contentString = '<div id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h1 id="firstHeading" class="firstHeading">'+string+'</h1>'+
+          '<div id="bodyContent">'+string1+
+          '</div>'+
+          '</div>';
+          var infowindow = new google.maps.InfoWindow({
+          content: contentString
+      });
+          myLatlng = new google.maps.LatLng(x,y);
+          var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: string
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+      });
+
+      });
+      });  //end of json
+
+  };
