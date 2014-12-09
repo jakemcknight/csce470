74af444fc68f31ebe3f38ed483f4baa24bad8a78
Miles Hickman <mileshick@gmail.com>
2014-12-09 12:36:03 -0600

    search is tokenized

--- app.py
+++ app.py
@@ -21,13 +21,17 @@ def get_by_zip(key, search):
     with open('scored_review.json') as f:
         businesses = (json.loads(f.readline()))
     in_zip = []
+    if search != ' ':
+        search_list = search.split(' ')
+    else:
+        search_list = [search]
     for val in businesses.values():
         res = val.get(key)
         if res is not None and search.lower() in res.lower():
             in_zip.append(val)
         if key.lower() == "zip":
             res = val.get('full_address')
-            if search.lower() in res.lower():
+            if any([x.lower() in res.lower() for x in search_list]):
                 in_zip.append(val)
     return jsonify(results=in_zip)
 
